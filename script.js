7ceff34dd9bda17292a6fa47c6aef67e5fa632e1
uneeth-Gaikwad <puneethgaikwadl@gmail.com>
2023-09-28 19:04:20 +0530

    formatting

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const header = document.getElementById("header");
 const body = document.getElementById("body");
-let rows = 0; 
+let rows = 0; //to keep count of number of rows dynamically
 
+// creating the rows containing A, B, C...
 for (let i = 64; i <= 90; i++) {
     if (i == 64) {
         const bold = document.createElement("b");
@@ -15,7 +16,7 @@ for (let i = 64; i <= 90; i++) {
     }
 }
 
-
+// adding cells dynamicaly
 function addCells() {
     const row = document.createElement("div");
     row.className = "row";
@@ -36,18 +37,83 @@ function addCells() {
     body.appendChild(row);
 }
 
+// function will be called to create rows
 function createRows(){
     for (let i = 1; i <= 100; i++) {
         addCells();
     }
 }
 
+// initial call for building 100 rows
 createRows();
 
+// function will be called when a cell is focused
+let activeCell = null;
 
 function onFocus(e){
-console.log(e.target.id);
 let cellName = document.getElementById("cellName");
+activeCell =  e.target;
 cellName.innerText = e.target.id;
 }
+
+const defaultOptionsState = {
+    fontFamily: "",
+    isBoldSelected: false,
+    isItalicSelected: false,
+    isUnderLineSelected: false,
+    textAlign: "left",
+    textColor: "#000",
+    backgroundColor: "#fff",
+    fontSize: 14,
+};
+
+const activeOptionState = {...defaultOptionsState};
+
+// function for toggling boldness
+function onClickBold(boldButton){
+
+    boldButton.classList.toggle("active");
+
+    if(activeCell){
+        if(activeOptionState.isBoldSelected === false){
+            activeCell.style.fontWeight = "600";
+            activeOptionState.isBoldSelected = true;
+        }else{
+            activeCell.style.fontWeight = "400";
+            activeOptionState.isBoldSelected = false;
+        }
+    }
+    
+}
+
+function onClickItalic(italicButton){
+
+    italicButton.classList.toggle("active");
+
+    if(activeCell){        
+        if(activeOptionState.isItalicSelected === false){
+            activeCell.style.fontStyle = "italic";
+            activeOptionState.isItalicSelected = true;
+        }else{
+            activeCell.style.fontStyle = "normal";
+            activeOptionState.isItalicSelected = false;
+        }
+    }
+    
+}
+
+function onClickUnderline(undelineButton){
+    undelineButton.classList.toggle("active");
+    
+    if(activeCell){
+        let textDecoration = getComputedStyle(activeCell).textDecoration;
+
+        if(textDecoration === "none"){
+            activeCell.style.textDecoration = "underline";
+        }else{
+            activeCell.style.textDecoration = "none";
+        }
+    }
+}
+
 
