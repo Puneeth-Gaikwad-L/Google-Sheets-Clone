59da21942b24dc80dd48a33147ecf11fda4ec5cc
uneeth-Gaikwad <puneethgaikwadl@gmail.com>
2023-09-30 10:46:58 +0530

    foratting

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let rows = 0; //to keep count of number of rows dynamically
 for (let i = 64; i <= 90; i++) {
     if (i == 64) {
         const bold = document.createElement("b");
-        bold.className="startCell";
+        bold.className = "startCell";
         header.appendChild(bold);
     } else {
         const bold = document.createElement("b");
@@ -38,7 +38,7 @@ function addCells() {
 }
 
 // function will be called to create rows
-function createRows(){
+function createRows() {
     for (let i = 1; i <= 100; i++) {
         addCells();
     }
@@ -47,73 +47,120 @@ function createRows(){
 // initial call for building 100 rows
 createRows();
 
-// function will be called when a cell is focused
-let activeCell = null;
+// will hold the start data on focus of a particular cell
+let activeOptionState;
 
-function onFocus(e){
-let cellName = document.getElementById("cellName");
-activeCell =  e.target;
-cellName.innerText = e.target.id;
+// function will be called when a cell is focused
+let activeCell;
+
+function onFocus(e) {
+    if (activeCell) {
+        activeCell.style.border = "1px solid #dbdbdb";
+    }
+    let cellName = document.getElementById("cellName");
+    activeCell = e.target;
+    activeCell.style.border = "2px solid blue";
+    cellName.innerText = e.target.id;
+
+    const computedStyle = getComputedStyle(activeCell);
+
+    // stores the formatting data related to the active cell
+    activeOptionState = {
+        fontFamily: computedStyle.fontFamily,
+        isBoldSelected: computedStyle.fontSize === "600",
+        isItalicSelected: computedStyle.fontFamily === "italic",
+        isUnderLineSelected: computedStyle.textDecoration === "underline",
+        textAlign: computedStyle.textAlign,
+        textColor: computedStyle.color,
+        backgroundColor: computedStyle.backgroundColor,
+        fontSize: computedStyle.fontSize,
+    };
 }
 
-const defaultOptionsState = {
-    fontFamily: "",
-    isBoldSelected: false,
-    isItalicSelected: false,
-    isUnderLineSelected: false,
-    textAlign: "left",
-    textColor: "#000",
-    backgroundColor: "#fff",
-    fontSize: 14,
-};
-
-const activeOptionState = {...defaultOptionsState};
-
 // function for toggling boldness
-function onClickBold(boldButton){
+function onClickBold(boldButton) {
 
     boldButton.classList.toggle("active");
 
-    if(activeCell){
-        if(activeOptionState.isBoldSelected === false){
+    if (activeCell) {
+        if (activeOptionState.isBoldSelected === false) {
             activeCell.style.fontWeight = "600";
-            activeOptionState.isBoldSelected = true;
-        }else{
+        } else {
             activeCell.style.fontWeight = "400";
-            activeOptionState.isBoldSelected = false;
         }
+        activeOptionState.isBoldSelected = !activeOptionState.isBoldSelected;
     }
-    
+
 }
 
-function onClickItalic(italicButton){
+// function for toggling italic
+function onClickItalic(italicButton) {
 
     italicButton.classList.toggle("active");
 
-    if(activeCell){        
-        if(activeOptionState.isItalicSelected === false){
-            activeCell.style.fontStyle = "italic";
-            activeOptionState.isItalicSelected = true;
-        }else{
+    if (activeCell) {
+        if (activeOptionState.isItalicSelected) {
             activeCell.style.fontStyle = "normal";
-            activeOptionState.isItalicSelected = false;
+        } else {
+            activeCell.style.fontStyle = "italic";
         }
+        activeOptionState.isItalicSelected = !activeOptionState.isItalicSelected;
     }
-    
+
 }
 
-function onClickUnderline(undelineButton){
+// function for toggling underline
+function onClickUnderline(undelineButton) {
     undelineButton.classList.toggle("active");
-    
-    if(activeCell){
-        let textDecoration = getComputedStyle(activeCell).textDecoration;
 
-        if(textDecoration === "none"){
-            activeCell.style.textDecoration = "underline";
-        }else{
+    if (activeCell) {
+        if (activeOptionState.isUnderLineSelected) {
             activeCell.style.textDecoration = "none";
+        } else {
+            activeCell.style.textDecoration = "underline";
+        }
+        activeOptionState.isUnderLineSelected = !activeOptionState.isUnderLineSelected;
+    }
+}
+// will have an arr of formating elements
+const textAlignElements = document.getElementsByClassName("text-align")
+// function for traversing the above arr and highlighting the required element
+function highlightTextAlignButton(textAlign) {
+    for (let i = 0; i < textAlignElements.length; i++) {
+        if (textAlignElements[i].getAttribute("data-value") === textAlign) {
+            textAlignElements[i].classList.add("active");
+        } else {
+            textAlignElements[i].classList.remove("active");
         }
     }
 }
+// function for aligning the text inside each cell
+function textAlign(textAlignButton) {
+    let selectedValue = textAlignButton.getAttribute("data-value");
 
+    //calling the function for highlighting the button
+    highlightTextAlignButton(selectedValue);
 
+    if (activeCell) {
+        activeCell.style.textAlign = selectedValue;
+        activeOptionState.textAlign = selectedValue;
+    }
+}
+
+// function for changing the text color
+function textColor(textColorInput) {
+    let selectedValue = textColorInput.value;
+    if (activeCell) {
+        activeCell.style.color = selectedValue;
+        activeOptionState.textColor = selectedValue;
+    }
+}
+
+function backgroundColor(backgroundInput) {
+    let selectedValue = backgroundInput.value;
+    console.log(selectedValue);
+    if (activeCell) {
+        activeCell.style.backgroundColor = selectedValue;
+        activeOptionState.backgroundColor = selectedValue;
+    }
+}
